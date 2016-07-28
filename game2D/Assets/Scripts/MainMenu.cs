f2ab4261dd5fccf4d8951ba59f6d3c04df9818e1
Christopher Huber <christopherbhuber@gmail.com>
2016-07-28 00:55:36 -0700

    added a new scene for the menu and a script as well

---
+++ game2D/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+    public GameObject MainMenuUI;
+    
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetButtonDown("NewGame"))
+        {
+            Application.LoadLevel(1);
+        }
+
+        if (Input.GetButtonDown("LoadGame"))
+        {
+            Application.LoadLevel(2);
+        }
+
+        if (Input.GetButtonDown("Options"))
+        {
+            Application.LoadLevel(3);
+        }
+
+        if (Input.GetButtonDown("ExitGame"))
+        {
+            Application.Quit();
+        }
+    
+	}
+}
