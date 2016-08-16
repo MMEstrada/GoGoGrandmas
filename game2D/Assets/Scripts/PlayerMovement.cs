c639a250cf1b294cbca785e3b90fc0f9b4447a2d
Christopher Huber <christopherbhuber@gmail.com>
2016-08-16 00:59:42 -0700

    added animation to gertrude

--- game2D/Assets/Scripts/PlayerMovement.cs
+++ game2D/Assets/Scripts/PlayerMovement.cs
@@ -11,14 +11,18 @@ public class PlayerMovement : MonoBehaviour {
     public bool canJump = true;
 
     private Rigidbody2D rb2d;
+    private Animator anim;
 
 	// Use this for initialization
 	void Start () {
         rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+        anim.SetFloat("Speed", Mathf.Abs(Input.GetAxis("Horizontal")));
 	
 	}
 
